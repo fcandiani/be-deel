cf359d0c6c26f33c5b65d35b98758e42ed88e806
Fernando Candiani <fernandocandiani@gmail.com>
2023-01-26 11:09:14 -0300

    fix: only return a contract only if it belongs to the profile calling (despite of beign a contractor or client)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { sequelize } = require('./model');
+const { Op } = require('sequelize');
 const { getProfile } = require('./middleware/getProfile');
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,18 @@ app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
 /**
- * FIX ME!
  * @returns contract by id
  */
 app.get('/contracts/:id', getProfile, async (req, res) => {
     const { Contract } = req.app.get('models');
     const { id } = req.params;
-    const contract = await Contract.findOne({ where: { id } });
+    const { profile } = req;
+    const contract = await Contract.findOne({
+        where: {
+            id,
+            [Op.or]: [{ ClientId: profile.id }, { ContractorId: profile.id }],
+        },
+    });
     if (!contract) return res.status(404).end();
     res.json(contract);
 });
